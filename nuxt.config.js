d5310c17aa6b5675a8b72692a33df7fec0775661
emacliam <mushyp1608@gmail.com>
2020-12-06 14:39:10 +0200

    changed the url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,113 +1,120 @@
-const URL = 'https://polar-retreat-14217.herokuapp.com'
+const URL = "https://pdfinder.herokuapp.com/";
 export default {
-    /*
-     ** Nuxt rendering mode
-     ** See https://nuxtjs.org/api/configuration-mode
-     */
-    mode: 'universal',
-    /*
-     ** Headers of the page
-     ** See https://nuxtjs.org/api/configuration-head
-     */
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-    /*
-     ** Global CSS
-     */
-    css: [],
-    /*
-     ** Plugins to load before mounting the App
-     ** https://nuxtjs.org/guide/plugins
-     */
-    plugins: [{
-            src: "~plugins/vue-js-modal.js",
-            ssr: false
-        },
-        { src: "~plugins/chart.js", ssr: false },
-        { src: "~plugins/hchs-vue-charts.js", ssr: false },
-        "~/plugins/vee-validate.js",
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    loading: '~/components/loading.vue',
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [
-        // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-        '@nuxtjs/tailwindcss',
-    ],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios',
-        '@nuxtjs/pwa',
-        // Doc: https://github.com/nuxt/content
-        '@nuxtjs/auth',
-        '@nuxtjs/toast'
-
-    ],
-
-    /*
-     ** Axios module configuration
-     ** See https://axios.nuxtjs.org/options
-     */
-    axios: {
-        proxy: true,
-        baseURL: URL
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [
+    {
+      src: "~plugins/vue-js-modal.js",
+      ssr: false
     },
-    proxy: {
-        /* configure this for auth coz if not the auth library will use a wrong localhost which is the default but not ours  */
-        "/api": URL
-    },
-    /*
-     ** Content module configuration
-     ** See https://content.nuxtjs.org/configuration
-     */
-    content: {},
-    /*
-     ** Build configuration
-     ** See https://nuxtjs.org/api/configuration-build/
-     */
-    build: {
-        transpile: ["vee-validate/dist/rules"],
+    { src: "~plugins/chart.js", ssr: false },
+    { src: "~plugins/hchs-vue-charts.js", ssr: false },
+    "~/plugins/vee-validate.js"
+  ],
+  loading: "~/components/loading.vue",
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    "@nuxtjs/tailwindcss"
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    // Doc: https://github.com/nuxt/content
+    "@nuxtjs/auth",
+    "@nuxtjs/toast"
+  ],
 
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {}
-    },
-    auth: {
-        strategies: {
-            local: {
-                endpoints: {
-                    login: { url: '/api/auth/login', method: 'post', propertyName: 'token' },
-                    /*  logout: { url: '/api/auth/logout', method: 'post' }, */
-                    logout: true,
-                    user: { url: '/api/auth/user', method: 'get', propertyName: 'user' }
-                },
-            }
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {
+    proxy: true,
+    baseURL: URL
+  },
+  proxy: {
+    /* configure this for auth coz if not the auth library will use a wrong localhost which is the default but not ours  */
+    "/api": URL
+  },
+  /*
+   ** Content module configuration
+   ** See https://content.nuxtjs.org/configuration
+   */
+  content: {},
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {
+    transpile: ["vee-validate/dist/rules"],
+
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  },
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: "/api/auth/login",
+            method: "post",
+            propertyName: "token"
+          },
+          /*  logout: { url: '/api/auth/logout', method: 'post' }, */
+          logout: true,
+          user: { url: "/api/auth/user", method: "get", propertyName: "user" }
         }
-    },
-    toast: {
-        position: 'top-center',
-        register: [ // Register custom toasts
-            {
-                name: 'my-error',
-                message: 'Sorry Enter Location first',
-                options: {
-                    type: 'error'
-                }
-            }
-        ]
+      }
     }
-}
+  },
+  toast: {
+    position: "top-center",
+    register: [
+      // Register custom toasts
+      {
+        name: "my-error",
+        message: "Sorry Enter Location first",
+        options: {
+          type: "error"
+        }
+      }
+    ]
+  }
+};
